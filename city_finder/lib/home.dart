a40e459aca0bbb1c012853170b4d62f58b74e3fc
samsausville <sps222@lehigh.edu>
2019-03-24 01:48:39 -0400

    Home page close to done, at a really good place right now

--- city_finder/lib/home.dart
+++ city_finder/lib/home.dart
@@ -1,8 +1,6 @@
-import 'package:city_finder/cards_section_alignment.dart';
 import 'package:city_finder/chatScreen.dart';
 import 'package:city_finder/tags.dart';
 import 'package:flutter/material.dart';
-import 'cards_section_draggable.dart';
 
 PageController _pageController;
 City city = new City();
@@ -25,7 +23,7 @@ class SwipeFeedPage extends StatefulWidget
   _SwipeFeedPageState createState() => new _SwipeFeedPageState();
 }
 
-class _SwipeFeedPageState extends State<SwipeFeedPage> {
+class _SwipeFeedPageState extends State<SwipeFeedPage> with TickerProviderStateMixin {
 
   var currentPageValue = 0.0;
   @override
@@ -81,17 +79,17 @@ class _SwipeFeedPageState extends State<SwipeFeedPage> {
                     transform: Matrix4.identity()
                       ..rotateY(currentPageValue - position)
                       ..rotateZ(currentPageValue - position),
-                    child: _buildPage()
+                    child: _buildProductDetailsPage(context),
                 );
               } else if (position == currentPageValue.floor() + 1) {
                 return Transform(
                     transform: Matrix4.identity()
                       ..rotateY(currentPageValue - position)
                       ..rotateZ(currentPageValue - position),
-                    child: _buildPage()
+                    child: _buildProductDetailsPage(context),
                 );
               } else {
-                _buildPage();
+                _buildProductDetailsPage(context);
               }
             },
             itemCount: 2,
@@ -99,110 +97,297 @@ class _SwipeFeedPageState extends State<SwipeFeedPage> {
       );
     }
 
-    _buildPage(){
-    return new Container(
-        decoration: new BoxDecoration(color: Colors.white,),
-        child: Stack(
+  _buildProductDetailsPage(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return ListView(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(4.0),
+          child: Card(
+            elevation: 4.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _buildProductImagesWidgets(),
+                _buildProductTitleWidget(),
+                SizedBox(height: 12.0),
+                _buildPriceWidgets(),
+                SizedBox(height: 12.0),
+                _buildDivider(screenSize),
+                SizedBox(height: 12.0),
+                _buildFurtherInfoWidget(),
+                SizedBox(height: 12.0),
+                _buildDivider(screenSize),
+                SizedBox(height: 12.0),
+                _buildSizeChartWidgets(),
+                SizedBox(height: 12.0),
+                _buildDetailsAndMaterialWidgets(),
+                SizedBox(height: 12.0),
+                _buildStyleNoteHeader(),
+                SizedBox(height: 6.0),
+                _buildDivider(screenSize),
+                SizedBox(height: 4.0),
+                _buildStyleNoteData(),
+                SizedBox(height: 20.0),
+                _buildMoreInfoHeader(),
+                SizedBox(height: 6.0),
+                _buildDivider(screenSize),
+                SizedBox(height: 4.0),
+                _buildMoreInfoData(),
+                SizedBox(height: 24.0),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  _buildDivider(Size screenSize) {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.grey[600],
+          width: screenSize.width,
+          height: 0.25,
+        ),
+      ],
+    );
+  }
+  _buildProductImagesWidgets() {
+    TabController imagesController = TabController(length: 3, vsync: this);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        height: 250.0,
+        child: Center(
+          child: DefaultTabController(
+            length: 3,
+            child: Stack(
+              children: <Widget>[
+                TabBarView(
+                  controller: imagesController,
+                  children: <Widget>[
+                    Image.asset("assets/Mpls.jpeg"),
+                    Image.asset("assets/mplsnite.jpg"),
+                    Image.asset("assets/person.png"),
+                  ],
+                ),
+                Container(
+                  alignment: FractionalOffset(0.5, 0.95),
+                  child: TabPageSelector(
+                    controller: imagesController,
+                    selectedColor: Colors.grey,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  _buildProductTitleWidget() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Center(
+        child: Text(
+          //name,
+          city.name,
+          style: TextStyle(fontSize: 16.0, color: Colors.black),
+        ),
+      ),
+    );
+  }
+  _buildPriceWidgets() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          /*ClipPath(
-            child: Container(color: Colors.black.withOpacity(0.8)),
-            clipper: getClipper(),
-          ),*/
-          ListView(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      city.name,
-                      style: TextStyle(
-                          color: Colors.orange,
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Montserrat'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Container(
-                        width:  MediaQuery.of(context).size.width,
-                        height: 300.0,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          image: DecorationImage(
-                              image: ExactAssetImage("assets/mplsnite.jpg"),
-                              fit: BoxFit.cover
-                          ),
-                        )),
-                  ),
-                  Container(
+          Text(
+            "87\% Match",
+            style: TextStyle(fontSize: 16.0, color: Colors.black),
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Text(
+            "Your Highest City Match",
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.blue[700],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
-                  ),
-                  SizedBox(height: 10.0),
-                  Padding(
-                    padding: EdgeInsets.all(25.0),
-                    child: Text(
-                      city.description,
-                      style: TextStyle(
-                          fontSize: 17.0,
-                          fontFamily: 'Montserrat'),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Text(
-                    "Matching Tags",
-                    style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Container(
-                      height: 100.0,
-                      width: 350.0,
-                      child: ListView.builder(
-                        itemCount: city.tags.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text('#${city.tags[index]}'),
-                          );
-                        },
-                      ),
-                    ),
-                  )
-                ],
-              )
+  _buildFurtherInfoWidget() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        children: <Widget>[
+          Icon(
+            Icons.local_offer,
+            color: Colors.grey[500],
+          ),
+          SizedBox(
+            width: 12.0,
+          ),
+          Text(
+            "#Young #Hot #Seasonal #Lakes #GoodVibes",
+            style: TextStyle(
+              color: Colors.grey[500],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  _buildSizeChartWidgets() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.straighten,
+                color: Colors.grey[600],
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Text(
+                "Population: 422,000",
+                style: TextStyle(
+                  color: Colors.grey[600],
+                ),
+              ),
             ],
           ),
         ],
-      )
+      ),
     );
+  }
+  _buildDetailsAndMaterialWidgets() {
+    TabController tabController = new TabController(length: 2, vsync: this);
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TabBar(
+            controller: tabController,
+            tabs: <Widget>[
+              Tab(
+                child: Text(
+                  "Description",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Companies",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
+            height: 60.0,
+            child: TabBarView(
+              controller: tabController,
+              children: <Widget>[
+                Text(
+                  city.description,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Great Place to live",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  _buildStyleNoteHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 12.0,
+      ),
+      child: Text(
+        "Tax Information",
+        style: TextStyle(
+          color: Colors.grey[800],
+        ),
+      ),
+    );
+  }
+  _buildStyleNoteData() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 12.0,
+      ),
+      child: Text(
+        "No income Tax",
+        style: TextStyle(
+          color: Colors.grey[600],
+        ),
+      ),
+    );
+  }
+  _buildMoreInfoHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 12.0,
+      ),
+      child: Text(
+        "Additonal Info",
+        style: TextStyle(
+          color: Colors.grey[800],
+        ),
+      ),
+    );
+  }
+  _buildMoreInfoData() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 12.0,
+      ),
+      child: Text(
+        "Friendly People",
+        style: TextStyle(
+          color: Colors.grey[600],
+        ),
+      ),
+    );
+  }
 
-    }
 
-
-    @override
+  @override
     void dispose() {
       super.dispose();
       _pageController.dispose();
     }
   }
-class getClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = new Path();
-
-    path.lineTo(0.0, size.height / 1.9);
-    path.lineTo(size.width + 125, 0.0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
-    return true;
-  }
-}
